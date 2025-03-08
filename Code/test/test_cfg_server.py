cb3952e672b1d4b92228af5128d5ed10c3c3b286
Andreas <andiburger@gmail.com>
2025-03-08 15:36:07 +0100

    refactor html cfg page; add css style file; added first tests

---
+++ Code/test/test_cfg_server.py
@@ -0,0 +1,34 @@
+import pytest
+import sys
+import os
+import requests
+import json
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+def test_cfg_server_empty_config(): 
+    url = 'http://127.0.0.1:5000/get_config'
+    response = requests.get(url)
+    if response.status_code == 200:
+        config_data = response.json() 
+        print(config_data)
+    else:
+        print(f"Error: {response.status_code}")
+    assert config_data == {
+        "MQTT Host": "",
+        "MQTT Port": "1883",
+        "MQTT Topic": "",
+        "MQTT Username": "",
+        "MQTT Password": "",
+        "Light Intensity min": "",
+        "Light Intensity max": "",
+        "Temperature min": "",
+        "Temperature max": "",
+        "Moisture min": "",
+        "Moisture max": ""
+    }
+
+
+
+if __name__ == '__main__':
+    test_cfg_server_empty_config()
