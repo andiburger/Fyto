96937e38d72f2e9e24dd7f6c52e80aa6381e8ee8
Andreas <andiburger@gmail.com>
2025-03-15 16:32:52 +0100

    calibration script changed to different input channels

--- Code/calibration.py
+++ Code/calibration.py
@@ -4,8 +4,14 @@ import time
 i2c = busio.I2C(board.SCL, board.SDA)
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+
 ads = ADS.ADS1115(i2c)
-chan = AnalogIn(ads, ADS.P2)
+Moisture_channel = AnalogIn(ads, ADS.P1)
+LDR_channel = AnalogIn(ads, ADS.P2)
+LM35_channel = AnalogIn(ads, ADS.P3)
 while True:
-   print(chan.value)
+   print(Moisture_channel.value)
+   print(LDR_channel.value)
+   print(LM35_channel.value)
    time.sleep(0.1)
+
