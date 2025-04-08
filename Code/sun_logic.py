f602493ff72bcb1e8d93de8ea62c55239998e5fc
Andreas <andiburger@gmail.com>
2025-04-08 18:08:23 +0200

    add timedelta to sun_logic

--- Code/sun_logic.py
+++ Code/sun_logic.py
@@ -1,15 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from astral.geocoder import database, lookup
 from astral.sun import sun
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+_LOGGER = logging.getLogger(__name__)
 
 def get_location(city_name):
     try:
         location = lookup(city_name, database())
         return location
     except KeyError:
-        print(f"Error: The city '{city_name}' was not found.")
+        _LOGGER.error(f"Error: The city '{city_name}' was not found.")
         return None
 
 def get_sun_times(city):
     s = sun(city.observer, date=datetime.now(), tzinfo=city.timezone)
-    return s["sunrise"].time(), s["sunset"].time()
+    # Apply 2-hour offset
+    sunrise = (s["sunrise"] + timedelta(hours=2)).time()
+    sunset = (s["sunset"] - timedelta(hours=2)).time()
+    return sunrise, sunset
